fe5506e042c1f647e1c0a7ecbbff1698e0512291
Yaron Vanhulst <kazaamjt@gmail.com>
2020-01-02 19:07:00 +0100

    Issue/30 add linting and jenkinsfile (#1)
    * Add Jenkinsfile, tests and linting
    
    * Fix formatting

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -15,8 +15,8 @@
 
     Contact: code@inmanta.com
 """
+from inmanta.agent.handler import ResourceHandler, provider
 from inmanta.resources import ResourceNotFoundExcpetion
-from inmanta.agent.handler import provider, ResourceHandler
 
 
 @provider("std::Service", name="ubuntu_service")
@@ -24,15 +24,21 @@ class UbuntuService(ResourceHandler):
     """
         A handler for services on systems that use upstart
     """
+
     def available(self, resource):
-        return not self._io.file_exists("/bin/systemctl") and (self._io.file_exists("/usr/lib/upstart") or self._io.file_exists("/usr/sbin/update-rc.d"))
+        return not self._io.file_exists("/bin/systemctl") and (
+            self._io.file_exists("/usr/lib/upstart")
+            or self._io.file_exists("/usr/sbin/update-rc.d")
+        )
 
     def check_resource(self, ctx, resource):
         current = resource.clone()
         style = ""
         if self._io.file_exists("/etc/init/%s.conf" % resource.name):
             # new style (upstart)
-            boot_config = self._io.run("/sbin/initctl", ["show-config", resource.name])[0]
+            boot_config = self._io.run("/sbin/initctl", ["show-config", resource.name])[
+                0
+            ]
             current.onboot = "start on " in boot_config
 
             exists = self._io.run("/sbin/status", [resource.name])
@@ -45,7 +51,12 @@ class UbuntuService(ResourceHandler):
 
         elif self._io.file_exists("/etc/init.d/%s" % resource.name):
             # old style
-            current.onboot = "already exist" in self._io.run("/usr/sbin/update-rc.d", ["-n", resource.name, "defaults"])[0]
+            current.onboot = (
+                "already exist"
+                in self._io.run(
+                    "/usr/sbin/update-rc.d", ["-n", resource.name, "defaults"]
+                )[0]
+            )
 
             if self._io.run("/etc/init.d/%s" % resource.name, ["status"])[2] == 0:
                 current.state = "running"
@@ -54,7 +65,9 @@ class UbuntuService(ResourceHandler):
 
             style = "init"
         else:
-            raise ResourceNotFoundExcpetion("The %s service does not exist" % resource.name)
+            raise ResourceNotFoundExcpetion(
+                "The %s service does not exist" % resource.name
+            )
 
         ctx.set("style", style)
         return current
@@ -77,7 +90,10 @@ class UbuntuService(ResourceHandler):
         # update-rc.d foobar defaults
         # update-rc.d -f foobar remove
 
-        if "state" in changes and changes["state"]["current"] != changes["state"]["desired"]:
+        if (
+            "state" in changes
+            and changes["state"]["current"] != changes["state"]["desired"]
+        ):
             action = "start"
             if changes["state"]["desired"] == "stopped":
                 action = "stop"
@@ -86,16 +102,23 @@ class UbuntuService(ResourceHandler):
             if style == "upstart":
                 result = self._io.run("/sbin/%s" % action, [resource.name])
                 if result[2] > 0:
-                    raise Exception("Unable to %s %s: %s" % (action, resource.name, result[1]))
+                    raise Exception(
+                        "Unable to %s %s: %s" % (action, resource.name, result[1])
+                    )
 
             elif style == "init":
                 result = self._io.run("/etc/init.d/%s" % resource.name, [action])
                 if result[2] > 0:
-                    raise Exception("Unable to %s %s: %s" % (action, resource.name, result[1]))
+                    raise Exception(
+                        "Unable to %s %s: %s" % (action, resource.name, result[1])
+                    )
 
             ctx.set_updated()
 
-        if "onboot" in changes and changes["onboot"]["current"] != changes["onboot"]["desired"]:
+        if (
+            "onboot" in changes
+            and changes["onboot"]["current"] != changes["onboot"]["desired"]
+        ):
             onboot = changes["onboot"]["desired"]
 
             if style == "upstart":
@@ -105,6 +128,8 @@ class UbuntuService(ResourceHandler):
                 if onboot:
                     self._io.run("/usr/sbin/update-rc.d", [resource.name, "defaults"])
                 else:
-                    self._io.run("/usr/sbin/update-rc.d", ["-f", resource.name, "remove"])
+                    self._io.run(
+                        "/usr/sbin/update-rc.d", ["-f", resource.name, "remove"]
+                    )
 
                 ctx.set_updated()
